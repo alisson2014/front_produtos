20b5bc7e3ad36a3770e17fd80c729fd855150499
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-04-27 10:18:45 -0300

    adicionei o react-bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,57 @@
+import React, { useState, useEffect } from "react"
+import { Container, Button } from "react-bootstrap";
+import Table from "react-bootstrap/Table";
+import "./css/app.css";
+
 export default function App() {
+  const [data, setData] = useState([]);
+
+  const requester = async () => {
+    fetch("http://localhost/produtosLike/index.php")
+      .then((response) => response.json())
+      .then((data) => (
+        setData(data["categorias"])
+      ))
+      .catch((e) => (
+        console.log(e)
+      ))
+      .finally(() => (
+        console.log("Fim da requisição.")
+      ))
+  }
+
+  useEffect(() => {
+    requester();
+  }, []);
+
   return (
-    <div className="App">
-      Olá
-    </div>
+    <Container>
+      <Table
+        striped
+        bordered
+        hover
+        variant="dark"
+      >
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Categoria</th>
+            <th className="actions">Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.values(data).map((categorie) => (
+            <tr key={categorie["id"]}>
+              <td>{categorie["id"]}</td>
+              <td>{categorie["nomeCategoria"]}</td>
+              <td className="actions">
+                <Button variant="primary">Editar</Button>
+                <Button variant="danger">Excluir</Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Container>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "bootstrap/dist/css/bootstrap.min.css";
 import App from './App';
+import "./css/global.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
