a12f31085d87d4a076fde9134e914a07d6bde5b9
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-05-27 16:31:51 -0300

    Finalizei o funcionamento da page produtos

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
-import { deleteFn, getData, useLocalStorage } from "service";
+import { getData, useLocalStorage } from "service";
 import { Table, Button } from "react-bootstrap";
 import { RiDeleteBin2Fill } from "react-icons/ri";
 import { BiEdit } from "react-icons/bi"
 import { Products as Form } from "components/Forms";
 import { Box, Title, Buttons } from "./styles";
 import { IProducts, localProducts, id } from "interface";
+import Swal from "sweetalert2";
+import { deleteData } from "service/delete";
 
 export default function Products() {
     const initialState: IProducts = {
@@ -41,6 +43,46 @@ export default function Products() {
         setPropsProduct(initialState);
         handleOpen();
     };
+
+    const deleteFn = (
+        id: id,
+        deleted: string,
+        typeData: string,
+        file: string
+    ) => {
+        Swal.fire({
+            title: `Deseja excluir ${deleted}?`,
+            text: "Você não poderá reverter isso!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#d33",
+            cancelButtonColor: "#3085d6",
+            confirmButtonText: "Sim, Deletar!",
+            cancelButtonText: "Cancelar",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                deleteData(file, id).then((res) => {
+                    if (res?.status) {
+                        Swal.fire(
+                            "Deletado!",
+                            `${typeData} deletada da base de dados.`,
+                            "success"
+                        ).then((res) => {
+                            if (res.isConfirmed) {
+                                setProducts(null);
+                                window.location.reload()
+                            };
+                        });
+                    } else {
+                        Swal.fire("Erro!", "Erro ao deletar na base de dados.", "error").then(
+                            (res) => res.isConfirmed && window.location.reload()
+                        );
+                    }
+                });
+            }
+        });
+    };
+
 
     useEffect(() => {
         if (products === null || products.length === 0) {
