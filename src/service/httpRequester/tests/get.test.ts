7490c25ec110bd6a396d544c637d8212acd9cabf
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-07-09 16:52:49 -0300

    Ajustei os testes de requisição

--- src/service/httpRequester/tests/get.test.ts
+++ src/service/httpRequester/tests/get.test.ts
@@ -4,10 +4,19 @@ describe("HTTP METHOD GET", () => {
   test("Deve trazer os dados da api", async () => {
     const response = await httpRequester({
       method: "GET",
-      file: "categories",
+      file: "categorias",
     });
     expect(response).toBeDefined();
   });
+
+  test("HTTP METHOD GET QUERY PARAM", async () => {
+    const response = await httpRequester({
+      method: "GET",
+      file: "categorias",
+      id: 35
+    });
+    expect(response).toBeDefined();
+  })
 
   test("Deve retornar um array vazio em caso de erro", async () => {
     const response = await httpRequester({
